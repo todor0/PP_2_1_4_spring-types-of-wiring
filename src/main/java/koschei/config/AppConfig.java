ef1347a45c0d9f37c5fafc0b591cd2485200eb34
todor0 <todorkin@gmail.com>
2023-01-08 18:27:48 +0500

    done

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,6 +1,8 @@
 package koschei.config;
 
+import koschei.models.Duck5;
 import koschei.models.Island2;
+import koschei.models.Rabbit4;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,5 +15,10 @@ public class AppConfig {
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+
+    @Bean
+    public static Rabbit4 getRabbit(Duck5 duck) {
+        return new Rabbit4(duck);
     }
 }
